9e2676ff1dc0b088ae1eb11fac1d95d646688b3c
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-09-08 22:58:22 -0400

    Removed redundant data

--- pages/index.js
+++ pages/index.js
@@ -48,27 +48,26 @@ export default function Home() {
 				your assets into ETH / USDT.
 			</SectionTitle>
 			<Benefits data={benefitOne} />
-			<Benefits imgPos="right" data={benefitTwo} />
-			<SectionTitle
+			{/* <Benefits imgPos="right" data={benefitTwo} /> */}
+			{/* <SectionTitle
 				pretitle="Watch a video"
 				title="Learn how to fullfil your needs"
 			>
 				This section is to highlight a promo or demo video of your product.
 				Analysts says a landing page with video has 3% more conversion rate. So,
 				don't forget to add one. Just like this.
-			</SectionTitle>
-			<Video />
-			<SectionTitle
+			</SectionTitle> */}
+			{/* <Video /> */}
+			{/* <SectionTitle
 				pretitle="Testimonials"
 				title="Here's what our customers said"
 			>
 				Testimonails is a great way to increase the brand trust and awareness.
 				Use this section to highlight your popular customers.
-			</SectionTitle>
-			<Testimonials />
+			</SectionTitle> */}
+			{/* <Testimonials /> */}
 			<SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
-				Answer your customers possible questions here, it will increase the
-				conversion rate as well as support or chat requests.
+				Some questions we get asked a lot.
 			</SectionTitle>
 			<Faq />
 			<Cta />
