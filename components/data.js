0f69e2889bc22bfdab22ef715a8cd84c3d68075e
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-09-08 22:58:50 -0400

    Changed benefitOne data

--- components/data.js
+++ components/data.js
@@ -1,59 +1,59 @@
 import {
-  EmojiHappyIcon,
-  ChartSquareBarIcon,
-  CursorClickIcon,
-  DeviceMobileIcon,
-  AdjustmentsIcon,
-  SunIcon,
+	EmojiHappyIcon,
+	ChartSquareBarIcon,
+	CursorClickIcon,
+	DeviceMobileIcon,
+	AdjustmentsIcon,
+	SunIcon,
 } from "@heroicons/react/outline";
 
 import benefitOneImg from "../public/img/benefit-one.png";
 import benefitTwoImg from "../public/img/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlight your benefits",
-  desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
-  image: benefitOneImg,
-  bullets: [
-    {
-      title: "Understand your customers",
-      desc: "Then explain the first point breifly in one or two lines.",
-      icon: <EmojiHappyIcon />,
-    },
-    {
-      title: "Improve acquisition",
-      desc: "Here you can add the next benefit point.",
-      icon: <ChartSquareBarIcon />,
-    },
-    {
-      title: "Drive customer retention",
-      desc: "This will be your last bullet point in this section.",
-      icon: <CursorClickIcon />,
-    },
-  ],
+	title: "Simple interface to get out of your assets",
+	desc: "Casual traders want to get out of their assets to ETH / USDT as fast as possible. Paperhands makes it easy to sell your assets at the best rates.",
+	image: benefitOneImg,
+	bullets: [
+		{
+			title: "Easy workflow to cash out",
+			desc: "Clean out your portfolio with a few clicks then cash out with one asset to a CEX.",
+			icon: <EmojiHappyIcon />,
+		},
+		{
+			title: "Improve your profit taking strategy",
+			desc: "Exit your assets to a stablecoin when you see the market getting shakey.",
+			icon: <ChartSquareBarIcon />,
+		},
+		{
+			title: "Easy to use on desktop and mobile",
+			desc: "With a few clicks you can jump into ETH or USDT with your assets.",
+			icon: <CursorClickIcon />,
+		},
+	],
 };
 
 const benefitTwo = {
-  title: "Offer more benefits here",
-  desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
-  image: benefitTwoImg,
-  bullets: [
-    {
-      title: "Mobile Responsive Template",
-      desc: "Nextly is designed as a mobile first responsive template.",
-      icon: <DeviceMobileIcon />,
-    },
-    {
-      title: "Powered by Next.js & TailwindCSS",
-      desc: "This template is powered by latest technologies and tools.",
-      icon: <AdjustmentsIcon />,
-    },
-    {
-      title: "Dark & Light Mode",
-      desc: "Nextly comes with a zero-config light & dark mode. ",
-      icon: <SunIcon />,
-    },
-  ],
+	title: "Offer more benefits here",
+	desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
+	image: benefitTwoImg,
+	bullets: [
+		{
+			title: "Mobile Responsive Template",
+			desc: "Nextly is designed as a mobile first responsive template.",
+			icon: <DeviceMobileIcon />,
+		},
+		{
+			title: "Powered by Next.js & TailwindCSS",
+			desc: "This template is powered by latest technologies and tools.",
+			icon: <AdjustmentsIcon />,
+		},
+		{
+			title: "Dark & Light Mode",
+			desc: "Nextly comes with a zero-config light & dark mode. ",
+			icon: <SunIcon />,
+		},
+	],
 };
 
 export { benefitOne, benefitTwo };
