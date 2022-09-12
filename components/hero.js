3f6e57c5c8312e009673cfca6e4991bb1178da25
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-09-11 21:49:31 -0400

    Added app to the access button

--- components/hero.js
+++ components/hero.js
@@ -21,7 +21,7 @@ export default function Hero() {
 
 						<div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
 							<a
-								href="/"
+								href="https://paperhands.vercel.app/"
 								target="_blank"
 								rel="noopener"
 								className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md "
