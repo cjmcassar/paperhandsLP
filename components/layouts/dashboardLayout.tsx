233ae87eaac3f6bb11722ab5e962459c4d6ae962
Mocarram Hossain <mocarramhossain@gmail.com>
2023-03-10 17:33:23 +0600

    added footer

--- components/layouts/dashboardLayout.tsx
+++ components/layouts/dashboardLayout.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Navbar from "../dashboard/navbar";
 import Sidebar from "../dashboard/sidebar/sidebar";
-// import Footer from "../dashboard/footer";
+import Footer from "../dashboard/footer";
 export default function DashboardLayout({ children }) {
   return (
-    <div>
+    <>
       <Sidebar />
 
-      <div className="relative md:ml-64 max-w-screen bg-black w-auto">
+      <div className="relative md:ml-64 bg-black w-auto">
         <Navbar />
       </div>
 
-      <div className="md:p-10 md:ml-64 max-w-screen">
-        {children}
+      <div className="md:p-10 md:ml-64 flex-1">{children}</div>
 
-        {/* <Footer /> */}
+      <div className="md:ml-64">
+        <Footer />
       </div>
-    </div>
+    </>
   );
 }
