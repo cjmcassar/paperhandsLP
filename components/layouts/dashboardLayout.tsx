367d0beca910c36d103a20a1108288f917f91499
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-03-11 20:53:40 -0500

    added left margin for new sidebar

--- components/layouts/dashboardLayout.tsx
+++ components/layouts/dashboardLayout.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import Navbar from "../dashboard/navbar";
 import Sidebar from "../dashboard/sidebar/sidebar";
+import RightHandSidebar from "../dashboard/sidebar/rightHandSidebar";
 import Footer from "../dashboard/footer";
 export default function DashboardLayout({ children }) {
-  return (
-    <>
-      <Sidebar />
+	return (
+		<>
+			<Sidebar />
 
-      <div className="relative md:ml-64 bg-black w-auto">
-        <Navbar />
-      </div>
+			<div className="relative md:ml-64 md:mr-64 bg-black w-auto">
+				<Navbar />
+			</div>
 
-      <div className="md:p-10 md:ml-64 flex-1">{children}</div>
+			<div className="md:p-10 md:ml-64 md:mr-64 flex-1">{children}</div>
 
-      <div className="md:ml-64">
-        <Footer />
-      </div>
-    </>
-  );
+			<RightHandSidebar />
+
+			<div className="md:ml-64 md:mr-64">
+				<Footer />
+			</div>
+		</>
+	);
 }
