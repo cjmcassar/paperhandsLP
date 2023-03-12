52b15c65d39ecdfe756e677122230cfec1430cf2
jeremydthomas <tommyhilfiger12@hotmail.com>
2023-03-11 22:16:22 -0600

    fix: fixed dom errors

--- components/dashboard/navbar.tsx
+++ components/dashboard/navbar.tsx
@@ -8,7 +8,6 @@ export default function Navbar() {
 	return (
 		<nav
 			className="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start"
-			navbar-main
 			navbar-scroll="false"
 		>
 			<div className="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
