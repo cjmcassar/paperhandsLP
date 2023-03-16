90b377081f85e8afff180c2ad5872a8f335096f6
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-03-15 20:12:25 -0400

    Update import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import Hero from "../components/hero";
-import Navbar from "../components/navbar";
-import SectionTitle from "../components/sectionTitle";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import SectionTitle from "../components/SectionTitle";
 
-import { benefitOne } from "../components/data";
-import Benefits from "../components/benefits";
-import Footer from "../components/footer";
-import Faq from "../components/faq";
+import { benefitOne } from "../components/Data";
+import Benefits from "../components/Benefits";
+import Footer from "../components/Footer";
+import Faq from "../components/Faq";
 
 export default function Home() {
 	return (
