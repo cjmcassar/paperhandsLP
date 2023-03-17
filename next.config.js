0c3486806197974e396a44cbca1d1d98351c951e
jeremydthomas <tommyhilfiger12@hotmail.com>
2023-03-17 11:37:07 -0500

    feat: add svg loader ability to make svg component

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  target: "serverless",
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+	target: "serverless",
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback.fs = false;
+		}
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: [
+				{
+					loader: "@svgr/webpack",
+					options: { icon: true },
+				},
+			],
+		});
+		return config;
+	},
 };
