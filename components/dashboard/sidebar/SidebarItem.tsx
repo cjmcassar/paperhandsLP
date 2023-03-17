10e3ff842ae7ded1ee824c423266c7719226dbb8
Mocarram Hossain <mocarramhossain@gmail.com>
2023-03-17 17:16:03 +0600

    feat(SidebarItem.tsx): add SidebarItem component to render a link with an icon and text, and highlight the active link based on the current route

---
+++ components/dashboard/sidebar/SidebarItem.tsx
@@ -0,0 +1,32 @@
+import { IconDefinition, IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useRouter } from "next/dist/client/router";
+
+function SidebarItem({
+  href,
+  children,
+  icon,
+}: {
+  href: string;
+  children: string;
+  icon: IconDefinition;
+}) {
+  const router = useRouter();
+  const isActive = router.pathname === href;
+  return (
+    <Link href={href}>
+      <a
+        href="#"
+        className={`transition duration-300 py-3 font-bold block ${
+          isActive ? "bg-purple" : "opacity-50 hover:opacity-100"
+        } px-5 rounded-xl`}
+      >
+        <FontAwesomeIcon className="pr-4" icon={icon as IconProp} />
+        {children}
+      </a>
+    </Link>
+  );
+}
+
+export default SidebarItem;
