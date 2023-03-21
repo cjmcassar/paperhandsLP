6b89e14ee27f9fd0400fca66ac41d0a0035ac080
Mocarram Hossain <mocarramhossain@gmail.com>
2023-03-22 04:53:02 +0600

    feat: holds the charts on the risk review page

---
+++ components/dashboard/risk-review/RiskLevelCharts.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DoughnutChart from "./DoughnutChart";
+
+function RiskLevelCharts() {
+  return (
+    <div className="my-6">
+      <DoughnutChart />
+    </div>
+  );
+}
+
+export default RiskLevelCharts;
