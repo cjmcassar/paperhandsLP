4d8ca133a183b3de27698d47a53ba0309ab556ed
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-03-20 23:41:11 -0400

    Refactored

--- components/dashboard/RiskInfo.tsx
+++ components/dashboard/RiskInfo.tsx
@@ -1,47 +1,51 @@
 import React from "react";
 
-const RiskInfo = () => {
-  return (
-    <div className="sm:w-full w-[30%] text-white">
-      <h1 className="text-2xl font-bold mb-4">Risk Information</h1>
-      <ul className="ml-3 flex flex-col items-start">
-        <li className="flex text-xs justify-between items-center mb-2">
-          <span className="bg-[#62FF97]  rounded-full h-[22.46px] w-[24px]"></span>
-          <span className="ml-3 flex flex-col ">
-            <h5>Low Risk Asset Bought</h5>
-            <p>July 4, 2023</p>
-          </span>
-        </li>
-        <li className="mb-2 flex text-xs justify-between items-center">
-          <span className="bg-[#FFF507] rounded-full h-[22.46px] w-[24px]"></span>
-          <span className="ml-3 flex flex-col ">
-            <h5>Medium Risk Asset Bought</h5>
-            <p>July 2, 2023</p>
-          </span>
-        </li>
-        <li className="mb-2 flex text-xs justify-between items-center">
-          <span className="bg-[#62FF97] rounded-full h-[22.46px] w-[24px]"></span>
-          <span className="ml-3 flex flex-col">
-            <h5>Low Risk Asset Sold</h5>
-            <p>June 4, 2023</p>
-          </span>
-        </li>
-        <li className="mb-2 flex text-xs justify-between items-center">
-          <span className="bg-[#FC62FF] rounded-full h-[22.46px] w-[24px]"></span>
-          <span className="ml-3 flex flex-col">
-            <h5>High Risk Asset Bought</h5>
-            <p>May 4, 2023</p>
-          </span>
-        </li>
-        <li className="mb-2 flex text-xs justify-between items-center">
-          <span className="bg-[#62FF97] rounded-full h-[22.46px] w-[24px]"></span>
-          <span className="ml-3 flex flex-col">
-            <h5>Low Risk Asset Sold</h5>
-            <p>April 4, 2023</p>
-          </span>
-        </li>
-      </ul>
-    </div>
-  );
-};
-export default RiskInfo;
+function RiskInfoItem({ color, title, date }) {
+	return (
+		<li className="flex text-xs justify-between items-center mb-2">
+			<span
+				className={`rounded-full h-[22.46px] w-[24px]`}
+				style={{ backgroundColor: color }}
+			></span>
+			<span className="ml-3 flex flex-col">
+				<h5>{title}</h5>
+				<p>{date}</p>
+			</span>
+		</li>
+	);
+}
+
+export default function RiskInfo() {
+	return (
+		<div className="sm:w-full w-[30%] text-white">
+			<h1 className="text-2xl font-bold mb-4">Risk Information</h1>
+			<ul className="ml-3 flex flex-col items-start">
+				<RiskInfoItem
+					color="#62FF97"
+					title="Low Risk Asset Bought"
+					date="July 4, 2023"
+				/>
+				<RiskInfoItem
+					color="#FFF507"
+					title="Medium Risk Asset Bought"
+					date="July 2, 2023"
+				/>
+				<RiskInfoItem
+					color="#62FF97"
+					title="Low Risk Asset Sold"
+					date="June 4, 2023"
+				/>
+				<RiskInfoItem
+					color="#FC62FF"
+					title="High Risk Asset Bought"
+					date="May 4, 2023"
+				/>
+				<RiskInfoItem
+					color="#62FF97"
+					title="Low Risk Asset Sold"
+					date="April 4, 2023"
+				/>
+			</ul>
+		</div>
+	);
+}
