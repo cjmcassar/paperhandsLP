8c39bcfb161ee029c1c3f6545c62be28f08e9e39
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-03-20 23:41:22 -0400

    Changed bg colour

--- components/layouts/DashboardLayout.tsx
+++ components/layouts/DashboardLayout.tsx
@@ -5,23 +5,23 @@ import RightHandSidebar from "../dashboard/sidebar/RightHandSidebar";
 
 import Footer from "../dashboard/Footer";
 export default function DashboardLayout({ children }) {
-  return (
-    <div>
-      <Sidebar />
+	return (
+		<div>
+			<Sidebar />
 
-      <div className="relative md:ml-72 md:mr-72 bg-phDarkGray w-auto">
-        <Navbar />
-      </div>
+			<div className="relative md:ml-72 md:mr-72 bg-phBlack w-auto">
+				<Navbar />
+			</div>
 
-      <div className="md:p-10 md:ml-72 md:mr-72 flex-1 bg-phBlack min-h-screen">
-        {children}
-      </div>
+			<div className="md:p-10 md:ml-72 md:mr-72 flex-1 bg-phBlack min-h-screen">
+				{children}
+			</div>
 
-      <RightHandSidebar />
+			<RightHandSidebar />
 
-      <div className="md:ml-72 md:mr-72">
-        <Footer />
-      </div>
-    </div>
-  );
+			<div className="md:ml-72 md:mr-72">
+				<Footer />
+			</div>
+		</div>
+	);
 }
