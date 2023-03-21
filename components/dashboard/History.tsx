e312c333df4e736346b049f2e17d861b0e6d248f
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-03-20 22:42:00 -0400

    Refactoed history

--- components/dashboard/History.tsx
+++ components/dashboard/History.tsx
@@ -3,88 +3,68 @@ import Eth from "../svg/eth.svg";
 import Matic from "../svg/matic.svg";
 import NFT from "../svg/nft.svg";
 
-export const History = () => {
+function HistoryItem({ icon, title, date, value, valueColor }) {
 	return (
-		<div className=" w-full text-white">
-			<h1 className=" font-bold mb-[17px] text-[24px] leading-[29px]">
+		<li className="flex mb-[23px]">
+			<div className="relative bg-[#393c4b] w-[51px] h-[51px] rounded-[20px]">
+				<div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+					{icon}
+				</div>
+			</div>
+			<div className="ml-[15px] flex justify-between items-center w-[200px]">
+				<div className="flex flex-col">
+					<h5 className="font-bold">{title}</h5>
+					<p className="text-sm">{date}</p>
+				</div>
+				<p className={valueColor}>{value}</p>
+			</div>
+		</li>
+	);
+}
+
+export default function History() {
+	return (
+		<div className="w-full text-white">
+			<h1 className="font-bold mb-[17px] text-[24px] leading-[29px]">
 				History
 			</h1>
 			<ul className="w-full">
-				<li className="flex mb-[23px]">
-					<div className="relative bg-[#393c4b] w-[51px] h-[51px] rounded-[20px]">
-						<div className="absolute left-[19.75px]  top-[14.75px] bottom-[15.75px]">
-							<Eth width="12.5px" height="20.5px" />
-						</div>
-					</div>
-
-					<div className="ml-[15px] flex justify-between items-center  w-[200px]">
-						<div className="flex flex-col ">
-							<h5 className="font-bold">Bought Eth</h5>
-							<p className="text-sm">July 4, 2023</p>
-						</div>
-						<p className="text-green-600 ">+1.55</p>
-					</div>
-				</li>
-				<li className="flex mb-[23px]">
-					<div className="relative  bg-[#393c4b] w-[51px] h-[51px] rounded-[20px]">
-						<div className="absolute left-[10px]  top-[9px] bottom-[10px] right-[9px]">
-							<Matic width="32px" height="32px" />
-						</div>
-					</div>
-
-					<div className="ml-[15px] flex justify-between items-center w-[200px]">
-						<div className="flex flex-col ">
-							<h5 className="font-bold">Bought Matic</h5>
-							<p className="text-sm">July 2, 2023</p>
-						</div>
-						<p className="text-green-600 ">+10.55</p>
-					</div>
-				</li>
-				<li className="flex mb-[23px] ">
-					<div className="relative  bg-[#393c4b] w-[51px] h-[51px] rounded-[20px]">
-						<div className="absolute left-[19.75px]  top-[14.75px] bottom-[15.75px]">
-							<Eth width="12.5px" height="20.5px" />
-						</div>
-					</div>
-					<div className="ml-[15px] flex justify-between items-center w-[200px]">
-						<div className="flex flex-col">
-							<h5 className="font-bold">Sold Eth</h5>
-							<p className="text-sm">June 4, 2023</p>
-						</div>
-						<p className="text-red-600 ">+1.55</p>
-					</div>
-				</li>
-				<li className="flex mb-[23px] ">
-					<div className="relative  bg-[#393c4b] w-[51px] h-[51px] rounded-[20px]">
-						<div className="absolute left-[14px]  top-[13px] bottom-[14.45px] right-[13.45px]">
-							<NFT fill="white" width="23.55px" height="23.54px" />
-						</div>
-					</div>
-					<div className="ml-[15px] flex justify-between items-center w-[200px]">
-						<div className="flex flex-col">
-							<h5 className="font-bold">Bought NFT</h5>
-							<p className="text-sm">May 4, 2023</p>
-						</div>
-						<p className="text-green-600 ">+1.55</p>
-					</div>
-				</li>
-				<li className="flex mb-[23px] ">
-					<div className="relative bg-[#393c4b] w-[51px] h-[51px] rounded-[20px]">
-						<div className="absolute left-[19.75px]  top-[14.75px] bottom-[15.75px]">
-							<Eth width="12.5px" height="20.5px" />
-						</div>
-					</div>
-					<div className="ml-[15px] flex justify-between items-center w-[200px]">
-						<div className="flex flex-col">
-							<h5 className="font-bold">Sold Eth</h5>
-							<p className="text-sm">June 4, 2023</p>
-						</div>
-						<p className="text-red-600 ">+1.55</p>
-					</div>
-				</li>
+				<HistoryItem
+					icon={<Eth width="12.5px" height="20.5px" />}
+					title="Bought Eth"
+					date="July 4, 2023"
+					value="+1.55"
+					valueColor="text-green-600"
+				/>
+				<HistoryItem
+					icon={<Matic width="32px" height="32px" />}
+					title="Bought Matic"
+					date="July 2, 2023"
+					value="+10.55"
+					valueColor="text-green-600"
+				/>
+				<HistoryItem
+					icon={<Eth width="12.5px" height="20.5px" />}
+					title="Sold Eth"
+					date="June 4, 2023"
+					value="+1.55"
+					valueColor="text-red-600"
+				/>
+				<HistoryItem
+					icon={<NFT fill="white" width="23.55px" height="23.54px" />}
+					title="Bought NFT"
+					date="May 4, 2023"
+					value="+1.55"
+					valueColor="text-green-600"
+				/>
+				<HistoryItem
+					icon={<Eth width="12.5px" height="20.5px" />}
+					title="Sold Eth"
+					date="June 4, 2023"
+					value="+1.55"
+					valueColor="text-red-600"
+				/>
 			</ul>
 		</div>
 	);
-};
-
-export default History;
+}
