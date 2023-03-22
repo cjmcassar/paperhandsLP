e9a1dc9f722ef0f8b8fea399f29c86d876ed289f
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-03-22 17:36:53 -0400

    Added css for the longer components

--- components/dashboard/risk-review/RiskReviewHeader.tsx
+++ components/dashboard/risk-review/RiskReviewHeader.tsx
@@ -4,30 +4,30 @@ import Plus from "../../svg/plus.svg";
 import Pen from "../../svg/pen.svg";
 import Question from "../../svg/question.svg";
 
+import styles from "./RiskReviewHeader.module.css";
+
 function RiskReviewHeader() {
-  return (
-    <div className="flex items-center py-5 gap-8">
-      <div>
-        <h2 className="capitalize text-white text-bold md:text-3xl sm:text-lg">
-          Risk Review
-        </h2>
-      </div>
-      <div className="flex gap-4 md:text-lg sm:text-xs text-white">
-        <button className="transition duration-300 flex items-center gap-3 rounded-full border-4 px-2 py-1 hover:border-phPurple">
-          <Plus width="22" height="22" />
-          <span className="text-xs">Add New Crypto</span>
-        </button>
-        <button className="transition duration-300 flex items-center gap-3 rounded-full border-4 px-2 py-1 hover:border-phPurple">
-          <Pen width="22" height="22" />
-          <span className="text-xs">Edit Portfolio</span>
-        </button>
-        <button className="transition duration-300 flex items-center gap-3 rounded-full border-4 px-2 py-1 hover:border-phPurple">
-          <Question width="22" height="22" />
-          <span className="text-xs">Help & FAQs</span>
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex items-center py-5 gap-8">
+			<div>
+				<h2 className={`${styles.latestOperationsTitle}`}>Risk Review</h2>
+			</div>
+			<div className={`${styles.buttonGroup}`}>
+				<button className={`${styles.customButton} hover:border-phPurple`}>
+					<Plus width="22" height="22" />
+					<span className="text-xs">Add New Crypto</span>
+				</button>
+				<button className={`${styles.customButton} hover:border-phPurple`}>
+					<Pen width="22" height="22" />
+					<span className="text-xs">Edit Portfolio</span>
+				</button>
+				<button className={`${styles.customButton} hover:border-phPurple`}>
+					<Question width="22" height="22" />
+					<span className="text-xs">Help & FAQs</span>
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default RiskReviewHeader;
