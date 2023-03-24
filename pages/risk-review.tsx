8a2140133d905fe5b753208b3786461dd0446d8e
jeremydthomas <tommyhilfiger12@hotmail.com>
2023-03-24 04:38:09 -0500

    fix: fixed an error

--- pages/risk-review.tsx
+++ pages/risk-review.tsx
@@ -1,6 +1,4 @@
 import RiskReview from "../components/dashboard/risk-review";
-import { getServerSideProps } from "./test";
-
 
 import DashboardLayout from "../components/layouts/DashboardLayout";
 function Risk() {
