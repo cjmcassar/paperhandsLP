338f08082c9a7608397124250616ffd27135dc19
jeremydthomas <tommyhilfiger12@hotmail.com>
2023-03-31 13:05:09 -0500

    feat: add new crypto button works

--- components/dashboard/risk-review/RiskLevelCharts.tsx
+++ components/dashboard/risk-review/RiskLevelCharts.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import DoughnutChart from "./DoughnutChart";
 
 function RiskLevelCharts() {
-  return (
-    <div className="my-6">
-      <DoughnutChart />
-    </div>
-  );
+	return (
+		<div className="my-6">
+			<DoughnutChart />
+		</div>
+	);
 }
 
 export default RiskLevelCharts;
--- pages/risk-review.tsx
+++ pages/risk-review.tsx
@@ -1,12 +1,27 @@
 import RiskReview from "../components/dashboard/risk-review";
 
 import DashboardLayout from "../components/layouts/DashboardLayout";
-function Risk() {
+
+function Risk({ assetData }) {
 	return (
 		<DashboardLayout>
-			<RiskReview />
+			<RiskReview assetData={assetData} />
 		</DashboardLayout>
 	);
 }
+export async function getStaticProps() {
+	const response1 = await fetch("http://localhost:3000/api/assets");
+	const assetData = await response1.json();
 
+	const response2 = await fetch("http://localhost:3000/api/storage");
+	const storageData = await response2.json();
+
+	return {
+		props: {
+			assetData: assetData.data,
+			storageData: storageData.data,
+		},
+		revalidate: 60,
+	};
+}
 export default Risk;
