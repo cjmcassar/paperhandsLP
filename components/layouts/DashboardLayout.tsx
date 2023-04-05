353ebde7c38b964126306ee776ff228ace3233ab
Mocarram Hossain <mocarramhossain@gmail.com>
2023-04-06 02:30:21 +0600

    feat: auth check barrier, auth checking state

--- components/layouts/DashboardLayout.tsx
+++ components/layouts/DashboardLayout.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../dashboard/Navbar";
 import Sidebar from "../dashboard/sidebar/Sidebar";
 import RightHandSidebar from "../dashboard/sidebar/RightHandSidebar";
+import { auth } from "../../utils/firebaseClient.js";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+import { useRouter } from "next/dist/client/router";
 
 import Footer from "../dashboard/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function DashboardLayout({ children }) {
-  return (
+  const router = useRouter();
+
+  const [loading, setLoading] = React.useState(true);
+
+  // Redirect to login page if user is not logged in
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        setLoading(false);
+      } else {
+        router.push("/login");
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return loading ? (
+    <div className="flex justify-center min-h-screen items-center text-2xl text-white bg-phBlack">
+      <FontAwesomeIcon icon={faSpinner} className="fa-spin mr-4" /> Checking
+      authentication
+    </div>
+  ) : (
     <div>
       <Sidebar />
 
