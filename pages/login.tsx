cf9201c9ec1a9cc0150cd7a8ec5a16d674ba6aa7
Mocarram Hossain <mocarramhossain@gmail.com>
2023-04-06 02:23:33 +0600

    feat: add login page

---
+++ pages/login.tsx
@@ -0,0 +1,15 @@
+import LoginModule from "../components/auth/LoginModule";
+import Navbar from "../components/landingpage/Navbar";
+import Footer from "../components/landingpage/Footer";
+
+function Login() {
+  return (
+    <>
+      <Navbar />
+      <LoginModule />
+      <Footer />
+    </>
+  );
+}
+
+export default Login;
