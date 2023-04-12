015f302a9fb078c857b537f00255f5a73e38cabb
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-04-12 11:40:31 -0700

    Removed unused import

--- components/dashboard/sidebar/components/SidebarItem.tsx
+++ components/dashboard/sidebar/components/SidebarItem.tsx
@@ -1,5 +1,3 @@
-import { IconDefinition, IconProp } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useRouter } from "next/dist/client/router";
 
