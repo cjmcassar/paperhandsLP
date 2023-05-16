1f50343ce1cf21e6bb943293cf939271f699c4f0
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-05-15 20:26:24 -0700

    added events

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,19 @@
+import DashboardLayout from "../components/layouts/DashboardLayout";
+
 import AssetSummary from "../components/dashboard/AssetSummary";
 import LineChart from "../components/dashboard/LineChart";
-import DashboardLayout from "../components/layouts/DashboardLayout";
+
 import { AssetDataProvider } from "../contexts/apiAssetDataContext";
 import { UserAssetsDataProvider } from "contexts/userAssetDataContext";
-import { UserTransactionsDataProvider } from "contexts/userTransactionDataContext"; // Import UserTransactionsDataProvider
+import { UserTransactionsDataProvider } from "contexts/userTransactionDataContext";
+
+import { analytics, logAppEvent } from "utils/firebaseClient";
 
 function Dashboard() {
+  if (typeof window !== "undefined") {
+    logAppEvent(analytics, "dashboard_page_visited");
+  }
+
   return (
     <DashboardLayout>
       <div>
--- pages/risk-review.tsx
+++ pages/risk-review.tsx
@@ -1,9 +1,17 @@
-import RiskReview from "../components/risk-review";
 import DashboardLayout from "../components/layouts/DashboardLayout";
+
+import RiskReview from "../components/risk-review";
+
 import { AssetDataProvider } from "../contexts/apiAssetDataContext";
 import { StorageDataProvider } from "contexts/apiStorageDataContext";
 
+import { analytics, logAppEvent } from "utils/firebaseClient";
+
 function Risk() {
+  if (typeof window !== "undefined") {
+    logAppEvent(analytics, "risk_review_page_visited");
+  }
+
   return (
     <DashboardLayout>
       <AssetDataProvider>
