0ffe69a1e6648974033e318247aff47aef200b83
Christopher-John Cassar <christopherjcassar@gmail.com>
2023-05-18 18:08:42 -0700

    Added functionality to the add crypto button

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,6 +1,7 @@
 import CryptoHistory from "../components/crypto-history";
 import DashboardLayout from "../components/layouts/DashboardLayout";
 import { AssetDataProvider } from "../contexts/apiAssetDataContext";
+import { StorageDataProvider } from "contexts/apiStorageDataContext";
 import { UserAssetsDataProvider } from "contexts/userAssetDataContext";
 import { UserTransactionsDataProvider } from "contexts/userTransactionDataContext";
 
@@ -8,11 +9,13 @@ function History() {
   return (
     <DashboardLayout>
       <AssetDataProvider>
-        <UserAssetsDataProvider>
-          <UserTransactionsDataProvider>
-            <CryptoHistory />
-          </UserTransactionsDataProvider>
-        </UserAssetsDataProvider>
+        <StorageDataProvider>
+          <UserAssetsDataProvider>
+            <UserTransactionsDataProvider>
+              <CryptoHistory />
+            </UserTransactionsDataProvider>
+          </UserAssetsDataProvider>
+        </StorageDataProvider>
       </AssetDataProvider>
     </DashboardLayout>
   );
